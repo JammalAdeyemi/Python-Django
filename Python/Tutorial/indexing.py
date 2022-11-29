2aee3d4fbf5b28f1010dd5d65748960ed916d596
Jammal <oabass7@gmail.com>
2022-11-29 21:47:38 +0000

    python

---
+++ Python/Tutorial/indexing.py
@@ -0,0 +1,26 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+print()
+# Using Negative slicing
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+print()
+## Slicing [star:stop:step]
+print(parrot[0:6]) # Norweg
+print(parrot[3:5]) # we
+print(parrot[:9]) # Norwegian
+print(parrot[10:14]) # Blue
+print(parrot[10:]) # Blue
+
+letters = "abcdefghijklmnopqrstuvwxyz"

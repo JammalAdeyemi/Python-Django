db55907970a5a2e9ad1ebe67a61e52277eace921
Jammal <oabass7@gmail.com>
2022-12-06 20:36:15 +0000

    python

--- Python/Tutorial/HyperionDev/Conditional_Statements/if_statements/optional_task.py
+++ Python/Tutorial/HyperionDev/Conditional_Statements/if_statements/optional_task.py
@@ -1,3 +1,22 @@
 #  Design a program for a department store to calculate the monthly wage for two different types of employees.
 
+print("Job role: \n1. Salesperson \n2. Manager ")
+user = int(input())
+
+# Salespeople earn an 8% commission on their gross sales and a fixed salary of R2 000.00 per month. Managers earn an hourly rate of R40.00
+
+# If the user is a salesperson, ask for their gross sales for the month.
+if user == 1:
+    sales = int(input("Input your gross sales for the month: "))
+    commission = sales * 0.08
+    fixed_salary = 2000
+    salary = fixed_salary + commission
+    print(f"\nTotal Salary for the month is {salary}")
+
+# If the user is a manager, ask for the number of hours worked for the month.
+elif user == 2:
+    hrs = int(input("Input the amount of hrs worked for the month: "))
+    hourly_rate = 40
+    salary_1 = hrs * hourly_rate
+    print(f"\nTotal Salary for the month is {salary_1}")
 
